d29949c3a0fe63545e82d23178611cb24f21615e
DonKongChys <106479354+DonKongChys@users.noreply.github.com>
2023-06-15 22:11:35 +0700

    adding stored

---
+++ src/main/java/uit/dayxahoi/racingbet/controller/StoredController.java
@@ -0,0 +1,83 @@
+package uit.dayxahoi.racingbet.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class StoredController {
+
+    @FXML
+    private Label HienThiTienLabel;
+
+    @FXML
+    private Label MapCoDienPane;
+
+    @FXML
+    private Button MuaMapCoDienButton;
+
+    @FXML
+    private Button MuaMapGayLuButton;
+
+    @FXML
+    private Button MuaMapNongBongButton;
+
+    @FXML
+    private Button MuaSkinDXHButton;
+
+    @FXML
+    private Button MuaSkinLichLamButton;
+
+    @FXML
+    private Button MuaSkinWibuButton;
+
+    @FXML
+    private Button QuayLaiButton;
+
+    @FXML
+    private Label SkinDXHPane;
+
+    @FXML
+    void muaMapCoDien(MouseEvent event) {
+        //MuaMapCoDienButton.setStyle("-fx-background-color: rgba(143,131,121,0.5);");
+        MuaMapCoDienButton.setDisable(true);
+    }
+
+    @FXML
+    void muaMapGayLu(MouseEvent event) {
+        MuaMapGayLuButton.setDisable(true);
+
+    }
+
+    @FXML
+    void muaMapNongBong(MouseEvent event) {
+        MuaMapNongBongButton.setDisable(true);
+
+    }
+
+    @FXML
+    void muaSkinDXH(MouseEvent event) {
+        MuaSkinDXHButton.setDisable(true);
+
+    }
+
+    @FXML
+    void muaSkinLichLam(MouseEvent event) {
+        MuaSkinLichLamButton.setDisable(true);
+
+    }
+
+    @FXML
+    void muaSkinWibu(MouseEvent event) {
+        MuaSkinWibuButton.setDisable(true);
+
+    }
+
+    @FXML
+    void quayLai(MouseEvent event) {
+        Stage stage = (Stage) QuayLaiButton.getScene().getWindow();
+        stage.close();
+    }
+
+}
