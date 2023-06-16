ae89d750f2bad5a0579a953c9fd2f78d7977991d
DonKongChys <106479354+DonKongChys@users.noreply.github.com>
2023-06-16 23:45:17 +0700

    bắt ngoại lệ login

--- src/main/java/uit/dayxahoi/racingbet/controller/LoginController.java
+++ src/main/java/uit/dayxahoi/racingbet/controller/LoginController.java
@@ -1,6 +1,7 @@
 package uit.dayxahoi.racingbet.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -22,12 +23,36 @@ public class LoginController {
 
     @FXML
     void dangKy(MouseEvent event) {
-
+        if(txtTenDN.getText().length() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Vui lòng nhập tên đăng nhập");
+            alert.setContentText("Vui lòng nhập tên đăng nhập");
+            alert.showAndWait();
+        } else if(txtPassword.getText().length() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Vui lòng nhập mật khẩu");
+            alert.setContentText("Vui lòng nhập mật khẩu");
+            alert.showAndWait();
+        }
     }
 
     @FXML
     void dangNhap(MouseEvent event) {
-
+        if(txtTenDN.getText().length() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Vui lòng nhập tên đăng nhập");
+            alert.setContentText("Vui lòng nhập tên đăng nhập");
+            alert.showAndWait();
+        } else if(txtPassword.getText().length() == 0) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Vui lòng nhập mật khẩu");
+            alert.setContentText("Vui lòng nhập mật khẩu");
+            alert.showAndWait();
+        }
     }
 
 }
