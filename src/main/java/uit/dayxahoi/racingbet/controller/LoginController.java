b917143b5412bfb480d19db5926c2f340c381091
DonKongChys <106479354+DonKongChys@users.noreply.github.com>
2023-06-16 23:38:32 +0700

    them front end login

---
+++ src/main/java/uit/dayxahoi/racingbet/controller/LoginController.java
@@ -0,0 +1,33 @@
+package uit.dayxahoi.racingbet.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button DangKyButton;
+
+    @FXML
+    private Button DangNhapButton;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtTenDN;
+
+    @FXML
+    void dangKy(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dangNhap(MouseEvent event) {
+
+    }
+
+}
